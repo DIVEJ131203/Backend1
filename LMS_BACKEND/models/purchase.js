90a7e72711a113c1dd3ec9714c4fa660ad5f6ded
Divej Ahuja <147057830+DIVEJ131203@users.noreply.github.com>
2025-03-18 23:05:16 +0530

    Purchase schema updated

--- LMS_BACKEND/models/purchase.js
+++ LMS_BACKEND/models/purchase.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
 const purchaseSchema = new mongoose.Schema({
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+        required: true
+    },
     userId: { type: String, required: true }, // Store as a string, not ObjectId
-    courseId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Course" },
-    amount: Number
-}, {timestamps: true });
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending'
+    }
+}, { timestamps: true });
 
 const Purchase = mongoose.model("Purchase", purchaseSchema);
 export default Purchase;
